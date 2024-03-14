a822d73fa07d4e085f750ce7c09aa05cd90dc4aa
Oscar Vian Valles <oscarvianvalles@gmail.com>
2024-03-14 13:02:11 +0800

    fix: build errors

--- config/webpack.plugins.js
+++ config/webpack.plugins.js
@@ -1,20 +1,20 @@
-const webpack = require('webpack');
-const cssnano = require('cssnano');
-const glob = require('glob');
-const path = require('path');
-const fs = require('fs');
+const webpack = require("webpack");
+const cssnano = require("cssnano");
+const glob = require("glob");
+const path = require("path");
+const fs = require("fs");
 
-const WebpackBar = require('webpackbar');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const StyleLintPlugin = require('stylelint-webpack-plugin');
-const WebappWebpackPlugin = require('webapp-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const RobotstxtPlugin = require('robotstxt-webpack-plugin');
-const SitemapPlugin = require('sitemap-webpack-plugin').default;
+const WebpackBar = require("webpackbar");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
+const WebappWebpackPlugin = require("webapp-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const RobotstxtPlugin = require("robotstxt-webpack-plugin");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
 
-const config = require('./site.config');
+const config = require("./site.config");
 
 // Hot module replacement
 const hmr = new webpack.HotModuleReplacementPlugin();
@@ -25,62 +25,60 @@ const optimizeCss = new OptimizeCssAssetsPlugin({
   cssProcessor: cssnano,
   cssProcessorPluginOptions: {
     preset: [
-      'default',
+      "default",
       {
         discardComments: {
-          removeAll: true,
-        },
-      },
-    ],
+          removeAll: true
+        }
+      }
+    ]
   },
-  canPrint: true,
+  canPrint: true
 });
 
 // Generate robots.txt
 const robots = new RobotstxtPlugin({
   sitemap: `${config.site_url}/sitemap.xml`,
-  host: config.site_url,
+  host: config.site_url
 });
 
 // Clean webpack
 const clean = new CleanWebpackPlugin();
 
-// Stylelint
-const stylelint = new StyleLintPlugin();
-
 // Extract CSS
 const cssExtract = new MiniCssExtractPlugin({
-  filename: 'style.[contenthash].css',
+  filename: "style.[contenthash].css"
 });
 
 // HTML generation
 const paths = [];
-const generateHTMLPlugins = () => glob.sync('./src/**/*.html').map((dir) => {
-  const filename = path.basename(dir);
+const generateHTMLPlugins = () =>
+  glob.sync("./src/**/*.html").map(dir => {
+    const filename = path.basename(dir);
 
-  if (filename !== '404.html') {
-    paths.push(filename);
-  }
+    if (filename !== "404.html") {
+      paths.push(filename);
+    }
 
-  return new HTMLWebpackPlugin({
-    filename,
-    template: path.join(config.root, config.paths.src, filename),
-    meta: {
-      viewport: config.viewport,
-    },
+    return new HTMLWebpackPlugin({
+      filename,
+      template: path.join(config.root, config.paths.src, filename),
+      meta: {
+        viewport: config.viewport
+      }
+    });
   });
-});
 
 // Sitemap
 const sitemap = new SitemapPlugin(config.site_url, paths, {
   priority: 1.0,
-  lastmodrealtime: true,
+  lastmodrealtime: true
 });
 
 // Favicons
 const favicons = new WebappWebpackPlugin({
   logo: config.favicon,
-  prefix: 'images/favicons/',
+  prefix: "images/favicons/",
   favicons: {
     appName: config.site_name,
     appDescription: config.site_description,
@@ -94,14 +92,14 @@ const favicons = new WebappWebpackPlugin({
       favicons: true,
       firefox: false,
       windows: false,
-      yandex: false,
-    },
-  },
+      yandex: false
+    }
+  }
 });
 
 // Webpack bar
 const webpackBar = new WebpackBar({
-  color: '#ff6469',
+  color: "#ff6469"
 });
 
 // Google analytics
@@ -113,32 +111,34 @@ class GoogleAnalyticsPlugin {
   }
 
   apply(compiler) {
-    compiler.hooks.compilation.tap('GoogleAnalyticsPlugin', (compilation) => {
+    compiler.hooks.compilation.tap("GoogleAnalyticsPlugin", compilation => {
       HTMLWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
-        'GoogleAnalyticsPlugin',
+        "GoogleAnalyticsPlugin",
         (data, cb) => {
-          data.html = data.html.replace('</head>', `${CODE.replace('{{ID}}', this.id) }</head>`);
+          data.html = data.html.replace(
+            "</head>",
+            `${CODE.replace("{{ID}}", this.id)}</head>`
+          );
           cb(null, data);
-        },
+        }
       );
     });
   }
 }
 
 const google = new GoogleAnalyticsPlugin({
-  id: config.googleAnalyticsUA,
+  id: config.googleAnalyticsUA
 });
 
 module.exports = [
   clean,
-  stylelint,
   cssExtract,
   ...generateHTMLPlugins(),
   fs.existsSync(config.favicon) && favicons,
-  config.env === 'production' && optimizeCss,
-  config.env === 'production' && robots,
-  config.env === 'production' && sitemap,
+  config.env === "production" && optimizeCss,
+  config.env === "production" && robots,
+  config.env === "production" && sitemap,
   config.googleAnalyticsUA && google,
   webpackBar,
-  config.env === 'development' && hmr,
+  config.env === "development" && hmr
 ].filter(Boolean);
